9c9428ee5dbeec1e38579b057fd08d68b90a80fc
janani_j <janani.j@dell.com>
2026-05-24 09:18:35 +0530

    edit add def

--- codebasic.py
+++ codebasic.py
@@ -1,11 +1,11 @@
 def main():
     print("Hello World")
 def add(a,b):
-    return a+b
+    return c
 if __name__ == "__main__":
     main()
-    add(10,20)
-    print(add(10,20))
+    c = add(10,20)
+    print(c)
 
 
 
